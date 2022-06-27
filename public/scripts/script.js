5433e9e7f74789f59aba4f6504401063f03e6d20
Mohamed Fadl <mohamed.fadl2852@gmail.com>
2022-06-27 17:04:51 +0200

    Added Uploaded File Preview Script

---
+++ public/scripts/script.js
@@ -0,0 +1,16 @@
+const filePickerEl = document.getElementById('avatar');
+const imagePreviewEl = document.getElementById('image-preview');
+
+const showPreview = () => {
+    const files = filePickerEl.files;
+    if (!files || files.length === 0) {
+        imagePreviewEl.style.display = 'none';
+        return;
+    }
+
+    const filePicked = files[0];
+    imagePreviewEl.src = URL.createObjectURL(filePicked);
+    imagePreviewEl.style.display = 'block';
+}
+
+filePickerEl.addEventListener('change', showPreview)
